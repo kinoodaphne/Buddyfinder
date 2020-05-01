d77b61b42e6bdd529bc403680fd4c5596ffc39e2
kinoodaphne <daphne.kinoo@outlook.be>
2020-05-01 20:54:28 +0200

    Added basic routes for the app
    - login
    - register
    - logout
    - students
    - students/{student}

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function() {
+    return view('login');
+});
+
+Route::get('/register', function() {
+    echo 'register page comes here';
+});
+
+Route::get('/logout', function() {
+    echo 'logout page comes here, redirect back to login page';
+});
+
+Route::get('/students/', function() {
+    echo 'An overview of all students comes here';
+});
+
+Route::get('/students/{student}', function() {
+    echo ' Detail profile of a student';
+});
