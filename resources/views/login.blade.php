7ac285fe4ecb3ea29bb7cb97d1e1a4e2e1814294
kinoodaphne <daphne.kinoo@outlook.be>
2020-05-02 17:16:28 +0200

    Created layout for login & register page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="images/rocket.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/images/rocket.png" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/app.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>IMDbuddy</title>
@@ -18,6 +18,7 @@
     <div class="container">
         <div class="sidenav">
             <div class="login-main-text">
+                <img src="/images/rocket.png" alt="logo" width="150px" heigh="150px">
                 <h2>IMDBUDDY<br> Login Page</h2>
                 <p>Login or register from here to access.</p>
             </div>
