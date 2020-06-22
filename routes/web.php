83a7e2bbf80c053405ad05199a19744544d04dc2
kinoodaphne <daphne.kinoo@outlook.be>
2020-06-22 11:24:06 +0200

    Update index
    Show students while on index

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StudentController@index');
 
 Route::get('/login', function() {
     return view('login');
