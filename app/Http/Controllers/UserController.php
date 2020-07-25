4b17ecee7742587f585d9e4738dc97a037d4812e
kinoodaphne <daphne.kinoo@outlook.be>
2020-07-25 17:04:05 +0200

    Added functionality so that logged in user is always displayed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,11 +28,23 @@ class UserController extends Controller
     public function handleLogin(Request $request) {
 
         $credentials = $request->only(['email', 'password']);
+
         if ( \Auth::attempt($credentials) ){
-            $user = new \App\User();
-            $request->session()->put('name', $user->name);
-            return redirect('/students');
+            $user = auth()->user();
+
+            $data['user'] = \App\User::find($user->id)->where('id', $user->id)->first()->name;
+
+            $name = $data['user'];
+
+            $request->session()->put('name', $name);
+            /**
+             * ->with is the same as flash
+             * return redirect('/')->with('name', $name);
+             *  */
+
+            return redirect('/');
         };
-        
+
+        return redirect()->route('login')->withErrors('Your email or password was incorrect!');        
     }
 }
