e5201d74ac383f5a0ea6604d982c65614953a434
kinoodaphne <daphne.kinoo@outlook.be>
2020-07-29 18:34:06 +0200

    Deleted password from StudentFactory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -10,7 +10,6 @@ $factory->define(\App\Student::class, function (Faker $faker) {
         'firstName' => $faker->firstName,
         'lastName' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         'year' => $faker->numberBetween(1,3),
         'study_field' => $faker->text,
         'music' => $faker->text,
