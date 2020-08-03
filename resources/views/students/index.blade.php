582392872309a95476957e5266dfbb2048a08c6e
kinoodaphne <daphne.kinoo@outlook.be>
2020-08-03 20:05:47 +0200

    Search component was actual data. No more repeated code

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -7,46 +7,6 @@
 @endcomponent
 
 @component('components/search')
-<section class="container">
-    <form action="" method="post">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="row">
-                    <div class="col-lg-3 col-md-3 col-sm-12 p-2">
-                        <select class="form-control search-slt" id="exampleFormControlSelect1">
-                            <option>Selecteer een kenmerk</option>
-                            <option>Gaming</option>
-                            <option>Muziek</option>
-                            <option>Hobbies</option>
-                            <option>Series</option>
-                            <option>Boeken</option>
-                            <option>Reizen</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 p-2">
-                        <select class="form-control search-slt" id="exampleFormControlSelect1">
-                            <option>Selecteer je keuzerichting</option>
-                            <option>Development</option>
-                            <option>Design</option>
-                            <option>Beide</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 p-2">
-                        <select class="form-control search-slt" id="exampleFormControlSelect1">
-                            <option>Selecteer een jaar</option>
-                            <option>1 IMD</option>
-                            <option>2 IMD</option>
-                            <option>3 IMD</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 p-2">
-                        <button type="button" class="btn btn-primary wrn-btn">Zoek</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-</section>
 @endcomponent
 
 @section('content')
