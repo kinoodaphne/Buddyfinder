6f8de658183a66ce1b76858b0aaf9dee5317c5bd
kinoodaphne <daphne.kinoo@outlook.be>
2020-08-04 18:12:31 +0200

    updated show.blade.php so that user can't edit this information

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -4,14 +4,14 @@
 @endcomponent
 
 @section('content')
-<h1>{{ $user->name }} {{ $user->lastName }}</h1>
 
 <hr>
 <div class="row">
     <!-- left column -->
     <div class="col-md-2">
         <div class="text-center">
-            <img src="{{ $user->profile_picture }}" class="avatar rounded-circle" alt="avatar" id="avatar" name="avatar" width="150" height="150">
+            <img src="{{ $user->profile_picture }}" class="avatar rounded-circle" alt="avatar" id="avatar" name="avatar"
+                width="150" height="150">
         </div>
     </div>
     <!-- edit form column -->
@@ -21,38 +21,44 @@
         <div class="form-group">
             <label class="col-lg-3 control-label">Voornaam:</label>
             <div class="col-lg-8">
-                <input readonly class="form-control-plaintext" type="text" value="{{ $user->name }}" name="name" id="name">
+                <input readonly class="form-control-plaintext" type="text" value="{{ $user->name }}" name="name"
+                    id="name">
             </div>
         </div>
         <div class="form-group">
             <label class="col-lg-3 control-label">Achternaam:</label>
             <div class="col-lg-8">
-                <input readonly class="form-control-plaintext" type="text" value="{{ $user->lastName }}" name="lastName" id="lastName">
+                <input readonly class="form-control-plaintext" type="text" value="{{ $user->lastName }}" name="lastName"
+                    id="lastName">
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-lg-3 control-label">Email:</label>
             <div class="col-lg-8">
-                <input readonly class="form-control-plaintext" type="text" value="{{ $user->email }}" name="email" id="email">
+                <input readonly class="form-control-plaintext" type="text" value="{{ $user->email }}" name="email"
+                    id="email">
             </div>
         </div>
         <div class="form-group">
             <label class="col-lg-3 control-label">Woonplaats:</label>
             <div class="col-lg-8">
-                <input readonly class="form-control-plaintext" type="text" value="{{ $user->location }}" name="location" id="location">
+                <input readonly class="form-control-plaintext" type="text" value="{{ $user->location }}" name="location"
+                    id="location">
             </div>
         </div>
         <div class="form-group">
             <label class="col-lg-3 control-label">Keuzerichting:</label>
             <div class="col-lg-8">
-                <input readonly class="form-control-plaintext" type="text" value="{{ $user->study_field }}" name="study_field" id="study_field">
+                <input readonly class="form-control-plaintext" type="text" value="{{ $user->study_field }}"
+                    name="study_field" id="study_field">
             </div>
         </div>
         <div class="form-group">
             <label class="col-lg-3 control-label">Korte omschrijving:</label>
             <div class="col-lg-8">
-                <textarea readonly class="form-control-plaintext" rows="8" id="biography" name="biography">{{ $user->bio }}</textarea>
+                <textarea readonly class="form-control-plaintext" rows="8" id="biography"
+                    name="biography">{{ $user->bio }}</textarea>
             </div>
         </div>
     </div>
